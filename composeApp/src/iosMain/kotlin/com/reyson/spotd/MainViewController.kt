de4059e4680f5951a2139981331dde177d356e00
JaysonMobileDev <jayson.mobile.dev@gmail.com>
2024-10-08 23:24:55 +0800

    init

---
+++ composeApp/src/iosMain/kotlin/com/reyson/spotd/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.reyson.spotd
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/wasmJsMain/kotlin/com/reyson/spotd/Platform.wasmJs.kt
@@ -0,0 +1,7 @@
+package com.reyson.spotd
+
+class WasmPlatform: Platform {
+    override val name: String = "Web with Kotlin/Wasm"
+}
+
+actual fun getPlatform(): Platform = WasmPlatform()
